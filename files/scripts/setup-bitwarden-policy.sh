fcef54900be8ead986402189f4fd49ac9434aac6
cvsickle <vansicklecorey@gmail.com>
2026-09-24 02:10:08 +0000

    removed unneeded bitwarden policy script

--- files/scripts/setup-bitwarden-policy.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-chown root:root /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy
-
-chcon system_u:object_r:usr_t:s0 /usr/share/polkit-1/actions/com.bitwarden.Bitwarden.policy
