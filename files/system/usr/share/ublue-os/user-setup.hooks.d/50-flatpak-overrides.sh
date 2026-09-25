a671fb9c97e72e986497e06972cd6029c353a566
cvsickle <vansicklecorey@gmail.com>
2026-09-25 02:05:07 +0000

    updated flatpak overrides

--- files/system/usr/share/ublue-os/user-setup.hooks.d/50-flatpak-overrides.sh
+++ files/system/usr/share/ublue-os/user-setup.hooks.d/50-flatpak-overrides.sh
@@ -3,9 +3,12 @@ set -euo pipefail
 
 # Allow flatpak access to themes.
 flatpak override --user \
-  --filesystem="$HOME/.themes" \
-  --filesystem="$HOME/.icons" \
-  --filesystem=xdg-config/gtk-4.0 \
+  --filesystem="$HOME/.themes:ro" \
+  --filesystem="$HOME/.local/share/themes:ro" \
+  --filesystem="$HOME/.icons:ro" \
+  --filesystem="$HOME/.local/share/icons:ro" \
+  --filesystem=xdg-config/gtk-3.0:ro \
+  --filesystem=xdg-config/gtk-4.0:ro \
   --filesystem=xdg-cache/DankMaterialShell:ro
 
 # Uninstall DankCalendar
