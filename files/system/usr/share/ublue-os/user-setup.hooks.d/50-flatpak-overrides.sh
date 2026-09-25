1e7adee179de080ac1b7d4de14735857b0585810
cvsickle <vansicklecorey@gmail.com>
2026-09-25 01:13:23 +0000

    added user script to remove dankcalendar flatpak

--- files/system/usr/share/ublue-os/user-setup.hooks.d/50-flatpak-overrides.sh
+++ files/system/usr/share/ublue-os/user-setup.hooks.d/50-flatpak-overrides.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
+# Allow flatpak access to themes.
 flatpak override --user \
   --filesystem="$HOME/.themes" \
   --filesystem="$HOME/.icons" \
   --filesystem=xdg-config/gtk-4.0
+
+# Uninstall DankCalendar
+if flatpak info com.danklinux.dankcalendar &>/dev/null; then
+    echo "Removing upstream Flatpak to prioritize native package..."
+    flatpak uninstall -y com.danklinux.dankcalendar
+fi
